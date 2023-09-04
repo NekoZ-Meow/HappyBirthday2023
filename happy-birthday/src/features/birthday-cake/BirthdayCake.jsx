53a5a10dcf0579fb93b8b818503a756056be997b
NekoZ-Meow <okc5424938d474w@gmail.com>
2023-09-04 23:31:28 +0900

    更新:2023-09-04-23-31-28

--- happy-birthday/src/features/birthday-cake/BirthdayCake.jsx
+++ happy-birthday/src/features/birthday-cake/BirthdayCake.jsx
@@ -3,6 +3,11 @@ import BirthdayCakeModel from '../../assets/models/birthdaycake.gltf'
 
 export const BirthdayCake = () => {
     return (
-        <Entity primitive='a-gltf-model' src={BirthdayCakeModel} position='0 -0.5 0' scale='0.015 0.015 0.015'></Entity>
+        <Entity
+            primitive='a-gltf-model'
+            src={BirthdayCakeModel}
+            position='0 -0.5 0.2'
+            scale='0.015 0.015 0.015'
+        ></Entity>
     )
 }
